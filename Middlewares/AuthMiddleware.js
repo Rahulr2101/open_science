af0de89eef03325cbc348817ffe68390e61f4d43
Pirajesh <pirajeshmr@gmail.com>
2023-10-08 12:02:05 +0530

    added user types

--- Middlewares/AuthMiddleware.js
+++ Middlewares/AuthMiddleware.js
@@ -1,19 +1,27 @@
 const User = require("../Models/UserModel");
-require("../.gitignore/node_modules/dotenv/lib/main").config();
 const jwt = require("jsonwebtoken");
 
 module.exports.userVerification = (req, res) => {
-  const token = req.cookies.token
+  const token = req.cookies.token;
   if (!token) {
-    return res.json({ status: false })
+    return res.json({ status: false });
   }
+  
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
     if (err) {
-     return res.json({ status: false })
+      return res.json({ status: false });
     } else {
-      const user = await User.findById(data.id)
-      if (user) return res.json({ status: true, user: user.username })
-      else return res.json({ status: false })
+      try {
+        const user = await User.findById(data.id);
+        if (user) {
+          return res.json({ status: true, username: user.username, userType: user.userType });
+        } else {
+          return res.json({ status: false });
+        }
+      } catch (error) {
+        console.error(error);
+        return res.json({ status: false });
+      }
     }
-  })
-}
+  });
+};
--- Models/UserModel.js
+++ Models/UserModel.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Your password is required"],
   },
+  userType: {
+    type: String,
+    enum: ["projectCreator", "contributor"],
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: new Date(),
